cdba0111706aef05eef85c92368a623f3656dd27
Nathan <nathan.rendall@gmail.com>
2021-06-30 21:29:32 +0100

    order class given additional properties, order.html organised

--- models/order.py
+++ models/order.py
@@ -1,11 +1,11 @@
 class Order():
 
-    def __init__(self, customer_name, order_date, quantity):
+    def __init__(self, customer_name, order_date, quantity, book_title, book_author, book_length):
         self.customer_name = customer_name
         self.order_date = order_date
         self.quantity = quantity
 
         # orders are books
-        self.book_title = ""
-        self.book_author = ""
-        self.book_length = 0
+        self.book_title = book_title
+        self.book_author = book_author
+        self.book_length = book_length
--- models/order_list.py
+++ models/order_list.py
@@ -1,8 +1,8 @@
 from models.order import *
 
-order_1 = Order("Nathan Rendall", "08/03/2021", 1)
-order_2 = Order("James Gough", "09/01/2021", 2)
-order_3 = Order("Stacey Mackie", "15/02/2021", 1)
+order_1 = Order("Nathan Rendall", "08/03/2021",1 , "Dune", "Frank Herbert", 689)
+order_2 = Order("James Gough", "09/01/2021", 2, "Hyperion", "Dan Simmons", 999)
+order_3 = Order("Stacey Mackie", "15/02/2021", 1, "Wizard of Earthsea", "Ursula Le Guin", 330)
 
 orders = [order_1, order_2, order_3]
 
